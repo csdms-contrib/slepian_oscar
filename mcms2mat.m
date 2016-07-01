2a89caff25d1c8bf4a5d1ac28ad3aa85eb68c994
Frederik J. Simons <fjsimons@gmail.com>
2016-07-01 10:10:46 -0400

    Small improvements to MCMS2MAT

--- mcms2mat.m
+++ mcms2mat.m
@@ -1,9 +1,15 @@
-function mcms2mat(yyyy,mm,dd,HH,MM,SS,qp,pdf)
-% MCMS2MAT(yyyy,mm,dd,HH,,MM,SS,qp,pdf)
+function mcms2mat(yyyy,mm,dd,HH,MM,SS,qp,pdf,of)
+% MCMS2MAT(yyyy,mm,dd,HH,MM,SS,qp,pdf,of)
 %
-% Quick look at the Meridian Compact PH instrument on PU Campus
-% Assumes a directory structure with MINISEED data, will combine
-% components, perform instrument correction, and save as MAT files. 
+% MeridianCompact-MiniSeed-to-MAT conversion of data files.
+% 
+% Queries a directory structure organized as $MC/YYYY/MM/DD/
+% within which, e.g. S0001.HHY_MC-PH1_0248_20160627_040000.miniseed 
+% data files as written by the Nanometrics instrument MC-PH1_0248. 
+% Unpacks using MSEED2SAC Version 2.0, a program available from 
+% https://seiscode.iris.washington.edu/projects/mseed2sac
+% [Performs instrument correction using SAC available from etc...]
+% Makes some plots, and saves as MAT files.
 %
 % INPUT:
 %
@@ -15,6 +21,8 @@ function mcms2mat(yyyy,mm,dd,HH,MM,SS,qp,pdf)
 % SS       Second [default: 00]
 % qp       Quick plots as we go along [default: 1 for yes]
 % pdf      Quick pdf print as we go along [default: 1 for yes]
+% of       1 Components saved in MAT file as separate variables [default] 
+%          2 Components saved in MAT files as cell entries
 %
 % USAGE:
 %
@@ -22,43 +30,50 @@ function mcms2mat(yyyy,mm,dd,HH,MM,SS,qp,pdf)
 % or leave the minutes and seconds, or even hours, out of it altogether
 % and it will apply to all available files, assuming hourly chunks.
 %
-% Last modified by fjsimons-at-alum.mit.edu, 05/15/2016
-
-% If future-timing problem perhaps "touch" all the files before going in?
+% SEE ALSO:
+%
+% MCGET (a tcsh shell script)
+%
+% Last modified by abrummen-at-princeton.edu, 07/01/2016
+% Last modified by fjsimons-at-alum.mit.edu, 07/01/2016
 
 % FIXED STUFF %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-% Data directory under which the yyyy/mm/dd directories are kept
-diro='/u/fjsimons/CLASSES/FRS-Spain/SensorData/MeridianCompactPH/';
+% Default data directory with the YYYY/MM/DD directories, set your own 'MC'
+setenv('MC',getenv('MC'))
 
 % Default directory where the EPS files will go, best to set your own 'EPS'
 setenv('EPS',getenv('EPS'))
 
-% Hard things such as our station, channel, instrument name etc
+% Hard things such as our station, channel, device name etc
 STA='S0001';
 CHA='HH%s';
 DEV='MC-PH1_0248';
 % Set of components we should be expecting for our miniseed files
 cmp={'X' 'Y' 'Z'};
 
-% Generic format of the MINISEED and MAT file names in those directories
+% Format of the MINISEED and MAT file names in those directories
 msfmt=sprintf('%s.%s_%s_%s.%s',STA,CHA,DEV,'%s','%s');
-% Generic SAC pre-format expected, might have to do it to find out
+% SAC format expected out of MSEED2SAC, had to run it to find out
 scfmt=sprintf('.%s..%s.%s.%s.%s.%s.SAC',STA,CHA,'D','%i','%i','%s');
+      
+% We may change our minds on this
+defval('of',2)
     
 % INPUT STUFF %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-% Defaults for the dates for which we have a data directory
-defval('yyyy',2015)
-defval('mm',10)
-defval('dd',13)
-% Set of hours we should be expecting for our miniseed files
+% Defaults for the dates for which we have a data directory... today!
+defval('yyyy',datestr(date,'yyyy'))
+defval('mm',datestr(date,'mm'))
+defval('dd',datestr(date,'dd'))
+% Set of hours we should be expecting for our miniseed files [all!]
 defval('HH',0:23)
-% Minutes and seconds that we expect these to start at
+% Minutes and seconds that we expect these to start at [zero!]
 defval('MM',0)
 defval('SS',0)
-% Make plots perhaps
+% Make a plot on the screen?
 defval('qp',1)
+% Save the plot as a pdf?
 defval('pdf',1)
 
 % Collect the Julian date already as it will be needed later
@@ -93,8 +108,8 @@ for index=1:length(HH)
   for ondex=1:length(cmp)
     % Make the MINISEED FILENAME with the precise time in it now also
     msx=fullfile(dirx,sprintf(msfmt,cmp{ondex},dst1,'miniseed'));
-    % Used for titles and plot names
-    mss=nounder(suf(msx,'/'),'\_');
+    % Used for titles and plot names... watch the underscore 
+    mss=nounder(suf(msx,'/'),'_');
     epsname=sprintf('%seps',pref(suf(msx,'/'),'miniseed'));
     pdfname=sprintf('%spdf',pref(suf(msx,'/'),'miniseed'));
     % Better test that the MINISEED exists as a filename
@@ -106,7 +121,8 @@ for index=1:length(HH)
       if exist(sax,'file')~=2
 	disp(sprintf('%s not found',sax)); 
         % What did it create last? Start timing could have been updated
-        % in the title. But note that 'last' means all other times are past
+        % in the title. But note that 'last' means all other times are PAST
+	% If "future-timing" encountered, "touch" all the files before going in
 	d=dir(pwd); [ds,n]=sort([d(:).datenum]);
 	sax=d(n(length(n))).name;
 	% We most likely want to go along with the file that it did create
@@ -115,6 +131,9 @@ for index=1:length(HH)
       end
       % Instrument response deconvolution? Header update in that case;
       % filename update also above, to do!
+      
+      % [INSTRUMENT CORRECTION]
+      
       % If plotting, get ready
       if qp==1; axes(ah(ondex)); end
       % Redefine sax again then READSAC and collect components
@@ -140,26 +159,34 @@ for index=1:length(HH)
     longticks(ah,2);
     xels=get(ah,'xlim'); xels=cat(1,xels{:});
     set(ah,'xlim',minmax(xels))
-    nolabels(ah(1:2))
+    nolabels(ah(1:2),1)
     for ondex=1:length(ah)
       axes(ah(ondex))
       movev(t{ondex},range(get(ah(ondex),'ylim'))/20)
     end
+    % Actually print to file? At least give a print suggestion! Force PDF
+    atmp=figdisp(epsname,[],[],~~pdf*2);
+    % Better move that plot to the working directory 
     if pdf==1
-      % Actually print to file, or at least give a filename suggestion
-      atmp=figdisp(epsname,[],[],pdf);
-      system(sprintf('ps2raster -Tf %s',atmp));
-      system(sprintf('rm -f %s',atmp));
-      % Better move that plot to the working directory 
       system(sprintf('mv -f %s %s',fullfile(getenv('EPS'),pdfname),dirx));
     end
   end
   % If you've been having success
   if exist('s')==1 && exist('h')==1
-    % Any components we have, save them together in a MAT file
-    save(mtx,'s','h')
+    switch of
+      case 1
+       % Any components we have, save them SEPARATELY in a MAT file
+       % The seismograms
+       sx=s{1}; sy=s{2}; sz=s{3};
+       % Their headers
+       hx=h{1}; hy=h{2}; hz=h{3};
+       % ...easier to subsequently direct-LOAD in, component by component 
+       save(mtx,'s','h','sx','sy','sz','hx','hy','hz')
+     case 2
+      % Any components we have, save them TOGETHER in a MAT file
+      save(mtx,'s','h')
+    end
     % Start the loop afresh
     clear s h
   end
 end
-
