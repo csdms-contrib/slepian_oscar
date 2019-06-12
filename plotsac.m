05767d2cd7d342b92fa0ef1d4d1a17979010b148
Frederik J. Simons <fjsimons@gmail.com>
2019-06-12 09:49:33 -0400

    A simple SAC seismogram plotting routine

--- plotsac.m
+++ plotsac.m
@@ -1,6 +1,5 @@
 function varargout=plotsac(SeisData,HdrData,varargin)
-% PLOTSAC(SeisData,HdrData,'Property','Value')
-% handle=PLOTSAC(SeisData,HdrData,'Property','Value')
+% [ph,xl,yl]=PLOTSAC(SeisData,HdrData,'Property','Value')
 %
 % INPUT
 %
@@ -12,23 +11,38 @@ function varargout=plotsac(SeisData,HdrData,varargin)
 %
 % OUTPUT
 %
-% handle          The plot handle or object
+% ph              The plot handle or object
+% tl              The title handle
+% xl              The x-label handle
+% yl              The y-label handle
 %
 % SEE ALSO
 %
 % READSAC, WRITESAC, MCM2SMAT
 %
-% Last modified by fjsimons-at-alum.mit.edu, 07/01/2016
+% Last modified by fjsimons-at-alum.mit.edu, 09/13/2017
+
+% Location from SAC2SAC, check with header
+lola=[-74.65475 40.34585];
 
 ph=plot(linspace(HdrData.B,HdrData.E,HdrData.NPTS),SeisData);
-xlabel([ 'time (s)'])
+tl=title(sprintf('recorded at Princeton University Guyot Hall %s (%7.3f%s,%7.3f%s)',deblank(HdrData.KSTNM),...
+		 lola(1),176,lola(2),176));
+
+yl=ylabel(sprintf('uncorrected %s component',...
+		  HdrData.KCMPNM));
+xl=xlabel(sprintf('time (s) since %4.4i (%3.3i) %2.2i:%2.2i:%2.2i.%3.3i',...
+		  HdrData.NZYEAR,HdrData.NZJDAY,...
+		  HdrData.NZHOUR,HdrData.NZMIN,HdrData.NZSEC,HdrData.NZMSEC));
 axis tight
 
 if ~isempty(varargin)
   setx(ph,varargin{:})
 end 
 
+longticks(gca,2)
+
 % Optional output
-varns={ph};
+varns={ph,tl,xl,yl};
 varargout=varns(1:nargout);
 
