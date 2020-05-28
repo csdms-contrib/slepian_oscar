4b4c95f17cccb0cee172e57c84493b0940adc90e
Frederik J. Simons <fjsimons@gmail.com>
2020-05-27 23:20:26 -0400

    Calculates and then nicely plots the spectrogram using SPECTROGRAM

--- timspecplot.m
+++ timspecplot.m
@@ -1,36 +1,64 @@
-function [p,xl,yl,bm,Bl10,F,T]=timspecplot(x,h,nfft,Fs,wlen,wolap,beg)
-% [p,xl,yl,bm,Bl10,F,T]=TIMSPECPLOT(x,h,nfft,Fs,wlen,wolap,beg)
+function [p,xl,yl,bm,Bl10,F,T]=timspecplot(x,h,nfft,Fs,wlen,wolap,beg,unt)
+% [p,xl,yl,bm,Bl10,F,T]=TIMSPECPLOT(x,h,nfft,Fs,wlen,wolap,beg,unt)
 %
-% Plots spectrogram of data using the SPECTROGRAM algorithm
+% Plots spectrogram of data using the SPECTROGRAM algorithm and maybe,
+% performs an analysis of it using BFT.  
 %
 % INPUT:
 % 
-% x        Signal - the actual data, e.g. from readsac
-% h        Header - the variables, e.g. from readsac
-% nfft     Number of FFT points
+% x        Signal - the actual data, e.g. from READSAC
+% h        Header - the variables, e.g. from READSAC
+% nfft     Number of FFT points [default: wlen]
 % Fs       Sampling frequency
-% wlen     Window length
-% wolap    Window overlap
+% wlen     Window length, in samples [default: 256]
+% wolap    Window overlap, as a fraction [default: 0.7]
 % beg      Signal beginning - actually, can get this from h
+% unt      String with the unit name [default: s]
+%
+% OUTPUT:
+%
+% p,xl,yl  Various axis handles
+% bm       A quality label from BFT, if so requested
+% Bl10     What's plotted, 10*log10(Ba2) with Ba2 the spectral density
+% F        Frequency axis (1/unt, which is Hz by default)
+% T        Time axis (unt), starting from zero
 %
 % Used by SIGNALS and SIGNALS2, see also
 % TIMDOMPLOT, SPECDENSPLOT
 %
-% Last modified by fjsimons-at-alum.mit.edu, 25.11.2004
+% Last modified by fjsimons-at-alum.mit.edu, 08/02/2012
+
+defval('beg',0)
+defval('wlen',256)
+defval('nfft',wlen)
+defval('wolap',0.7)
+defval('unt','s')
 
 % This is the calculation; the rest is plotting
-[Ba2,F,T,Bl10]=spectrogram(x,nfft,Fs,wlen,ceil(wolap*wlen));
+[Ba2,F,T,Bl10]=spectrogram(x,nfft,Fs,wlen,ceil(wolap*wlen),unt);
 
 % Conform to PCHAVE, SPECTRAL DENSITY, NOT POWER
 p=imagesc(beg+wlen/Fs/2+T,F,Bl10);
 axis xy; colormap(jet)    
-xlabs=sprintf('%s ; %3.1f s window','Time (s)',wlen/Fs);
-yl=ylabel(sprintf(...
-    '%s ; fN %5.1f ; fR %6.2e','Frequency (Hz)',...
-    Fs/2,Fs/wlen));
 
+% Labeling
+xfreq=sprintf('time (%s)',unt);
+
+if strcmp(unt,'s')
+  yfreq='frequency (Hz)';
+  tlabs='spectral density (energy/Hz)';
+else
+  yfreq=sprintf('frequency (%s^{-1})',unt);
+  tlabs=sprintf('spectral density (energy %s %s)','\times',unt);
+end
+
+xlabs=sprintf('%s ; %g %s window',xfreq,wlen/Fs,unt);
+ylabs=sprintf('%s ; fN %5.1f ; fR %6.2e',yfreq,...
+	    Fs/2,Fs/wlen);
+
+% Analysis; cut this out altogether if this isn't relevant
 ifisayso=0;
-if h.T0~=-12345 & h.T1~=-12345 & ifisayso==1
+if ifisayso==1 &&  h.T0~=-12345 && h.T1~=-12345
   hold on
   yli=ylim;
   pt01=plot(repmat([h.T0 h.T1],2,1),[yli(1) yli(2)],'k--');
@@ -42,5 +70,9 @@ if h.T0~=-12345 & h.T1~=-12345 & ifisayso==1
 else
   bm=[];
 end
-xl=xlabel(xlabs);
 
+% Put the labels on
+xl=xlabel(xlabs);
+yl=ylabel(ylabs);
+tl=title(sprintf('%s ; window size %g %s ; overlap %g %s ',...
+	tlabs,wlen/Fs,unt,ceil(wolap*wlen)/Fs,unt));
