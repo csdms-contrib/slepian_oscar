f48574fc8ad8174c9a0cc71fd38c56b1a965c393
Frederik J. Simons <fjsimons@gmail.com>
2020-05-27 23:02:53 -0400

    Calculates and then nicely plots the spectral density using PCHAVE

--- specdensplot.m
+++ specdensplot.m
@@ -1,16 +1,17 @@
-function varargout=specdensplot(x,nfft,Fs,lwin,olap,sfax)
-% [p,xl,yl,F,SD,Ulog,Llog]=SPECDENSPLOT(x,nfft,Fs,lwin,olap,sfax)
+function varargout=specdensplot(x,nfft,Fs,lwin,olap,sfax,unt)
+% [p,xl,yl,F,SD,Ulog,Llog]=SPECDENSPLOT(x,nfft,Fs,lwin,olap,sfax,unt)
 %
 % Plots spectral density of data calculated using the PCHAVE algorithm.
 %
 % INPUT:
 % 
 % x        Signal
-% nfft     Number of FFT points [default: length(x)]
+% nfft     Number of FFT points [default: lwin]
 % Fs       Sampling frequency
-% lwin     Window length [default: 256]
-% olap     Window overlap [default: 70]
+% lwin     Window length, in samples [default: 256]
+% olap     Window overlap, in percent [default: 70]
 % sfax     Y-axis scaling factor [default: 10]
+% unt      String with the unit name [default: s]
 %
 % OUTPUT:
 %
@@ -30,14 +31,15 @@ function varargout=specdensplot(x,nfft,Fs,lwin,olap,sfax)
 % 
 % SIGNALS, SIGNALS2, TIMDOMPLOT, TIMSPECPLOT
 %
-% Last modified by fjsimons-at-alum.mit.edu, 04/23/2009
+% Last modified by fjsimons-at-alum.mit.edu, 08/02/2012
 
-defval('nfft',length(x))
-defval('sfax',10)
 defval('lwin',256)
+defval('nfft',lwin)
 defval('olap',70)
+defval('sfax',10)
+defval('unt','s')
 
-% This is the calculation
+% This is the calculation; the rest is plotting
 [SD,F,Ulog,Llog]=pchave(x,lwin,olap,nfft,Fs);
 
 % The rest is simply for plotting cosmetically
@@ -52,10 +54,15 @@ xlim([F(1) F(end)]);
 % the plot, nothing funny should happen...
 
 % Note that \Delta F is the first frequency to actually show up in the plot
-xl=xlabel(sprintf('%s ; %i s window','frequency (Hz)',ceil(lwin/Fs)));
-yl=ylabel(sprintf('%s %5.2f',...
-		     'spectral density (energy/Hz) ; \Delta\itf=',...
-		     Fs/nfft));
+if strcmp(unt,'s')
+  xfreq='frequency (Hz)';
+  yfreq='spectral density (energy/Hz)';
+else
+  xfreq=sprintf('frequency (%s^{-1})',unt);
+  yfreq=sprintf('spectral density (energy %s %s)','\times',unt);
+end
+xl=xlabel(sprintf('%s ; %g %s window',xfreq,lwin/Fs,unt));
+yl=ylabel(sprintf('%s ; %s = %5.2f',yfreq,'\Delta\itf',Fs/nfft));
 mima=[F(2) F(end)];
 poslab=10.^[-3:3];
 set(gca,'Xtick',poslab(poslab>=mima(1) & poslab<=mima(2)));
@@ -63,4 +70,3 @@ set(gca,'Xtick',poslab(poslab>=mima(1) & poslab<=mima(2)));
 % Provide output of what's being plotted exactly
 vars={p,xl,yl,F,sfax*log10(SD),sfax*log10(Ulog),sfax*log10(Llog)};
 varargout=vars(1:nargout);
-
