56d744b49fd82bf73ca3531e65f03d3f1e0fae2e
Frederik J. Simons <fjsimons@gmail.com>
2021-01-20 17:44:03 -0500

    Date to Julian day conversion

--- dat2jul.m
+++ dat2jul.m
@@ -5,9 +5,9 @@ function julday=dat2jul(month,day,year)
 %
 % INPUT:
 %
-% month   The month
-% day     The day
-% year    The year
+% month   The month (a number)
+% day     The day (a number)
+% year    The year (a number)
 % 
 % OUTPUT:
 %
@@ -17,11 +17,14 @@ function julday=dat2jul(month,day,year)
 %
 % 8.3.0.532 (R2014a) and 9.0.0.314360 (R2016a)
 %
-% Last modified by fjsimons-at-alum.mit.edu, 07/31/2019
+% Last modified by fjsimons-at-alum.mit.edu, 02/21/2021
 
 % Default values are today!
-defval('month',str2num(datestr(today, 5)))
-defval('day',  str2num(datestr(today, 7)))
+%defval('month',str2num(datestr(today, 5)))
+defval('month',str2num(datestr(now, 5)))
+%defval('day',  str2num(datestr(today, 7)))
+defval('month',str2num(datestr(now, 7)))
+%defval('year', str2num(datestr(today,10)))
 defval('year', str2num(datestr(today,10)))
 
 % Do the calculation!
