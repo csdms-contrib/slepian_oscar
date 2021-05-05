cac0b077395678e53f7de7bcd8d3180fa50708de
Frederik J. Simons <fjsimons@gmail.com>
2021-05-05 17:55:22 -0400

    Finds intersection of a cylinder with a sphere

---
+++ cylindric.m
@@ -0,0 +1,121 @@
+function varargout=cylindric(r,toplola,botlola,R,xver)
+% varargout=CYLINDRIC(r,toplola,botlola,R,xver)
+%
+% Finds the interection of a cylinder of radius r with a sphere of radius R
+%
+% INPUT:
+%
+% r         Radius of the cylinder
+% toplola   Geographic coordinates of its top
+% botlola   Geographic coordinates of its bottom
+% R         Radius of the sphere         
+% xver      1 Extra verification by plotting
+%           0 No extra verification or plotting
+%
+% OUTPUT:
+%
+% xyzS      A cell array with the intersection points
+% 
+% EXAMPLES:
+%
+%% A random worked example and a plot
+% cylindric([],[],[],[],1)
+%
+%% A specific example and a plot example
+% xyzS=cylindric(0.1,[],[],1);
+% for index=1:length(xyzS)
+%   for ondex=1:size(xyzS{index},2)
+%       plot3(xyzS{index}(1,ondex),xyzS{index}(2,ondex),xyzS{index}(3,ondex),'k.')
+%       hold on 
+%     end
+% end
+% hold off; grid on ; axis([-1 1 -1 1 -1 1])
+%
+% SEE ALSO:
+%
+% POLECIRCLE, LINE3SPHERE, VIVIANI
+%
+% Last modified by fjsimons-at-alum.mit.edu, 05/05/2021
+
+% Defaults
+defval('toplola',rand(1,2).*[360 180]+[0 -90])
+defval('botlola',rand(1,2).*[360 180]+[0 -90])
+defval('r',0.1)
+defval('R',1)
+defval('xver',0)
+
+% Convert axis of cylinder to coordinates on the target sphere
+[xt,yt,zt]=sph2cart(toplola(1)*pi/180,toplola(2)*pi/180,R);
+[xb,yb,zb]=sph2cart(botlola(1)*pi/180,botlola(2)*pi/180,R);
+% Find center of cylinder
+xc=xb+[xt-xb]/2;
+yc=yb+[yt-yb]/2;
+zc=zb+[zt-zb]/2;
+[thc,phc]=cart2sph(xc,yc,zc);
+% Calculate the circumference of the cylinder at the top
+[th,ph]=cart2sph(xt-xc,yt-yc,zt-zc);
+% Remember the empty argument sets the resolution
+[xtc,ytc,ztc]=polecircle([th ph]*180/pi,[xt-xc yt-yc zt-zc r],[],-1);
+xtc=xtc+xc; ytc=ytc+yc; ztc=ztc+zc;
+% Calculate the circumference of the cylinder at the bottom
+[th,ph]=cart2sph(xb-xc,yb-yc,zb-zc);
+% Remember the empty argument sets the resolution
+[xbc,ybc,zbc]=polecircle([th ph]*180/pi,[xb-xc yb-yc zb-zc r],[],-1);
+% Need to flip since angles where measured in different direction
+xbc=flipud(xbc+xc); ybc=flipud(ybc+yc); zbc=flipud(zbc+zc);
+
+for index=1:length(xbc)
+  % Now find the intersections of the wall lines with the sphere
+  xyzS{index}=line3sphere([xtc(index) ytc(index) ztc(index)],...
+			  [xbc(index) ybc(index) zbc(index)],[0 0 0 R],...
+			  0);
+end
+
+% Optional plotting
+if xver==1 && R==1
+  % Make a plot, viewed centered on the center
+  [~,pe]=plotcont([],[],11,[],[],[thc*180/pi phc*180/pi]);
+  hold on
+  % Plot axis
+  pa=plot3([xt xb],[yt yb],[zt zb],'b');
+  % Plot center of axis
+  pc=plot3(xc,yc,zc,'b+');
+  % Plot top and bottom circles
+  pt=plot3(xtc,ytc,ztc,'r');
+  pb=plot3(xbc,ybc,zbc,'g');
+  % Plot some connecting lines... just a subset
+  for index=1:10:max(10,length(xbc))
+    pl(index)=plot3([xtc(index) xbc(index)],[ytc(index) ybc(index)],...
+		    [ztc(index) zbc(index)],'k');
+  end
+  % And all intersection points
+  for index=1:max(10,length(xbc))
+    for ondex=1:size(xyzS{index},2)
+      plot3(xyzS{index}(1,ondex),xyzS{index}(2,ondex),xyzS{index}(3,ondex),'k.')
+    end
+  end
+
+  hold off
+
+  % Now I need to figure out where these "walls" intersect the sphere
+  % https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+
+  % Cosmetics
+  xlabel('x')
+  ylabel('y')
+  zlabel('z')
+  set([pa pb pt],'LineW',2)
+
+  set(gca,'CameraViewAngle',9)
+  
+  % Last minute cleanup
+  delete([pt pb pl])
+  
+  % Save the plot
+  figdisp([],sprintf('%i_%i_%i_%i_%4.2f',round(toplola),round(botlola),r),[],1)
+end
+
+% Optional output
+varns={xyzS};
+varargout=varns(1:nargout);
+
