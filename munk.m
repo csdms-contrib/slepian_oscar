60aba9e1f22d7448973fb87c7abfcbc2819fb535
Frederik J. Simons <fjsimons@gmail.com>
2021-05-25 22:54:12 -0400

    A Munk sound speed profile for the ocean

--- munk.m
+++ munk.m
@@ -1,18 +1,22 @@
-function [cz,z]=munk(zm,zc,dz)
-% [cz,z]=munk(zm,zc,dz)
+function [cz,z,zc,B]=munk(zm,zc,dz,B)
+% [cz,z,zc,B]=munk(zm,zc,dz,B)
 %
-% Makes the Munk sound speed profile (10.1121/1.1914492)
+% Makes the Munk (1979) sound speed profile (10.1121/1.1914492)
+% also quoted by MIT Open Course Ware 2-S998 (Spring 2012, Lab 05)
 %
 % INPUT: 
 %
 % zm     The maximum depth (positive m)
 % zc     The depth of the minimum speed (m)
 % dz     The depth discretization (m) 
+% B      The Munk (1979) scale depth parameter (m)
 %
 % OUTPUT:
 %
 % cv     The sound speed at the requested depth
 % z      The requested depths (m)
+% zc     The depth of the minimum speed (m)
+% B      The Munk (1979) scale depth parameter (m)
 %
 % EXAMPLE:
 %
@@ -24,6 +28,7 @@ function [cz,z]=munk(zm,zc,dz)
 % Default values
 defval('zm',5000)
 defval('zc',1300)
+defval('B',1300)
 defval('dz',10)
 
 % Make the depths
@@ -31,7 +36,7 @@ z=0:dz:zm;
 
 % The sound speed profile
 epsilon=0.00737;
-zbar=2*(z-zc)/zc;
+zbar=2*(z-zc)/B;
 cz=1500*[1+epsilon*(zbar-1+exp(-zbar))];
 
 
