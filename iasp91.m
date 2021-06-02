6de8aad89e6fa8f356de2ae551ff77f96ab5d09d
Frederik J. Simons <fjsimons@gmail.com>
2021-06-02 15:36:19 -0400

    Interpolates Earth model iasp91 and its gradient

--- iasp91.m
+++ iasp91.m
@@ -1,14 +1,15 @@
 function val=iasp91(rad,fld,arg)
 % val=iasp91(rad,fld,arg)
 %
-% Interpolates the IASP91 Earth model without discontinuities
+% Interpolates the IASP91 Earth model pretreated to smooth over
+% discontinuities, and its radial gradient
 %
 % INPUT:
 %
 % rad       Radius [m]
 % fld       1 P-velocity [m/s]
 %           2 S-velocity [m/s]
-%           3 Density [kg/m^3]
+%           3 density [kg/m^3]
 % arg       1 need the field
 %           2 need the field gradient d(fld)/d(radius)
 %
@@ -16,11 +17,15 @@ function val=iasp91(rad,fld,arg)
 %
 % val       The interpolated fields or gradients
 %
+% SEE ALSO:
+%
+% AK135, PREMISO, EARTHMODEL, MODPREP
+%
 % Last modified by fjsimons-at-alum.mit.edu, 06/02/2021
 
 % Specify where you keep them
 defval('ddir',fullfile(getenv('IFILES'),'EARTHMODELS','MATFILES'))
-% Load data
+% Load specially prepared data which contain radius, psd, psdgrad
 load(fullfile(ddir,'iasp91'))
 
 % Perform the interpolation without thinking about discontinuities
