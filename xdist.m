34f6c9741181f0f87cf33c8d4610fb249f51e2bc
Frederik J. Simons <fjsimons@gmail.com>
2023-03-17 00:05:33 -0400

    The properly normalized XCORR as a distance measure

--- xdist.m
+++ xdist.m
@@ -1,26 +1,27 @@
-function [x,lags]=xdist(a,b,lags)
-% [x,lags]=XDIST(a,b,lags)
+function [x,lags]=xdist(a,b,lags,nm)
+% [x,lags]=XDIST(a,b,lags,nm)
 %
 % Lagged normalized cross-correlation between two vectors, e.g. time
 % series, i.e. the "multiplicative" version of the "difference" signal
 % similarity measure RDIST, i.e. the cross-correlation between two
 % vectors normalized by their cross-correlation at zero lag, in the
-% area of overlap. One could think of normalizing using the zero-lag
-% autocorrelation of either the first or the second signal, or in
-% terms of square root of the products of these autocorrelations at
-% zero lag, the latter of which is the only option supported here,
-% making this measure a truly shifted correlation coefficient - if you
-% take care of demeaning the signals also... Note that the signals are
-% NOT demeaned, if you want that, that is your job, as it's a choice
-% with consequences. The difference with XCORR option 'corr' is that
-% the normalization there is NOT recomputed in the area of overlap,
-% but calculated once at the start, which is appropriate for
-% stationary signals.
+% area of overlap, which is demeaned. One could think of normalizing
+% using the zero-lag autocorrelation of either the first or the second
+% signal, or in terms of square root of the products of these
+% autocorrelations at zero lag, the latter of which is the only option
+% supported here, making this measure a truly shifted correlation
+% coefficient. The difference with XCORR option 'corr' is that the
+% normalization there is NOT recomputed in the area of overlap, and
+% the segments are not demeaned, but calculated once at the start,
+% which is appropriate for stationary zero-mean signals. See also CORR
+% and CORRCOEF.
 %
 % INPUT:
 %
 % a,b         Two equal-length vectors (shortest will be zero-padded)
 % lags        The lags at which the measure is to be computed (defaulted)
+% nm          1 overlapped portions are individually demeaned [default]
+%             0 overrides the demeaning
 %
 % OUTPUT:
 %
@@ -32,7 +33,10 @@ function [x,lags]=xdist(a,b,lags)
 % 
 % SEE ALSO: RDIST, XCORR, and OST
 %
-% Last modified by fjsimons-at-alum.mit.edu, 02/22/2023
+% Last modified by fjsimons-at-alum.mit.edu, 03/16/2023
+
+% If 1, which should be default, individually sequentially demeaned
+defval('nm',1)
 
 % Only vectors, same length or zero-pad
 a=a(:);
@@ -55,11 +59,14 @@ x=nan(length(lags),1);
 % Do the computation, "for" loop might be slower but vectorization costs memory
 i=0;
 for l=lags
-  i=i+1;
-  x(i)=sum([b(1-l*[l<0]:end-l*[l>0]).*a(1+l*[l>0]:end+l*[l<0])])...
-  /sqrt(sum(b(1-l*[l<0]:end-l*[l>0]).^2))...
-  /sqrt(sum(a(1+l*[l>0]:end+l*[l<0]).^2));
+    i=i+1;
+    % The index into b of the interval of overlap
+    blap=1-l*[l<0]:M-l*[l>0];
+    % The index into a of the interval of overlap
+    alap=1+l*[l>0]:M+l*[l<0];
+    % Demean the individual segments, maybe overridden
+    bm=mean(b(blap))*nm; am=mean(a(alap))*nm;
+    % The ab-sqrt-normalized cross-correlation
+    x(i)=sum([b(blap)-bm].*[a(alap)-am])...
+         /sqrt(sum([b(blap)-bm].^2))/sqrt(sum([a(alap)-am].^2));
 end
-
-
-
