a73f29a7121e5a00a388d4b8b62f23e4b69309ba
Frederik Simons <fjsimons@alum.mit.edu>
2023-06-06 00:03:26 -0400

    Lunge forward

--- swspeed.m
+++ swspeed.m
@@ -1,5 +1,5 @@
-function c=swspeed(P,T,S,meth)
-% c=SWSPEED(P,T,S,meth)
+function c=swspeed(P,T,S,meth,DP,DT,DS)
+% c=SWSPEED(P,T,S,meth,DP,DT,DS)
 %
 % Ocean sound speed in m/s given pressure, temperature and salinity.
 %
@@ -13,6 +13,11 @@ function c=swspeed(P,T,S,meth)
 %           adjustment to Chen and Millero (1977, doi: 10.1121/1.381646)
 %         3 Inputs are standard deviations of T and S and in turn the
 %           output is standard deviation of the sound speed
+%         4 Del Grosso in a different implementation arrangement
+%         5 Standard deviations are input and output is one also
+% DP      Standard deviation of pressure [decibar]
+% DT      Standard deviation of temperature [degrees Celsius]
+% DS      Standard deviation of salinity [parts per thousand]
 %
 % OUTPUT:
 %
@@ -88,44 +93,79 @@ switch meth
     % Standard deviation inputs and outputs
     % Convert input in decibar to bar for this calculation
     P=P/10;
-    dcds=0;
+
     % From Yifeng
-    error('Needs to be reviewed and document')
+    error('Needs to be reviewed and documented')
+
     C1=3.1464e-9;
-    C2=-1.478e-6-(6.1185e-10).*pres+(1.0405e-12).*pres.^2;
-    C3=(-2.5335e-10).*pres.^2+(1.3621e-7).*pres+3.342e-4;
-    C4=(-2.3643e-12).*pres.^3+(2.5974e-8).*pres.^2-(8.1788e-6).*pres-5.8055e-2-(2.59e-8).*pres.^2+(1.4e-5).*pres;
-    C5=(3.8504e-10).*pres.^3-(1.7107e-6).*pres.^2+(6.8982e-4).*pres+5.03711+(3.47e-7).*pres.^2-(2.19e-4).*pres;
-    % Temperature derivative of the constants?
-    dcdt=5*temp.^4.*C1+4*temp.^3.*C2+3*temp.^2.*C3+2*temp.*C4+temp.*C5;
+    C2=-1.478e-6-(6.1185e-10).*P+(1.0405e-12).*P.^2;
+    C3=(-2.5335e-10).*P.^2+(1.3621e-7).*P+3.342e-4;
+    C4=(-2.3643e-12).*P.^3+(2.5974e-8).*P.^2-(8.1788e-6).*P-5.8055e-2-(2.59e-8).*P.^2+(1.4e-5).*P;
+    C5=(3.8504e-10).*P.^3-(1.7107e-6).*P.^2+(6.8982e-4).*P+5.03711+(3.47e-7).*P.^2-(2.19e-4).*P;
+    % Temperature derivative in decreasing powers of T
+    dCdT=5*T.^4.*C1+4*T.^3.*C2+3*T.^2.*C3+2*T.*C4+T.*C5;
 
-    A1=-3.389e-13.*temp.^2+6.649e-12.*temp+1.1e-10;
-    A2=7.988e-12.*temp.^3-1.6002e-10.*temp.^2+9.1041e-8.*temp-3.9064e-7;
-    A3=-2.0122e-10.*temp.^4+1.0507e-8.*temp.^3-6.4885e-8.*temp.^2-1.258e-5.*temp+9.4742e-5;
-    A4=-3.21e-8*temp.^4+2.006e-6*temp.^3+7.164e-5*temp.^2-1.262e-2*temp+1.389;
-    % Pressure derivative of the salinity constants?
-    dasalds=A1.*pres.^3+A2.*pres.^2+A3.*pres+A4;
+    A1=-3.389e-13.*T.^2+6.649e-12.*T+1.1e-10;
+    A2=7.988e-12.*T.^3-1.6002e-10.*T.^2+9.1041e-8.*T-3.9064e-7;
+    A3=-2.0122e-10.*T.^4+1.0507e-8.*T.^3-6.4885e-8.*T.^2-1.258e-5.*T+9.4742e-5;
+    A4=-3.21e-8*T.^4+2.006e-6*T.^3+7.164e-5*T.^2-1.262e-2*T+1.389;
+    % Pressure derivative in decreasing powers of P
+    dASdS=A1.*P.^3+A2.*P.^2+A3.*P+A4;
 
-    A5=-2.0122e-10*pres-3.21e-8;
-    A6=7.988e-12*pres.^2+1.0507e-8.*pres+2.006e-6;
-    A7=-3.389e-13.*pres.^3-1.6002e-10.*pres.^2-6.4885e-8*pres+7.184e-15;
-    A8=6.649e-12*pres.^3+9.1041e-8*pres.^2-1.258e-5*pres-1.262e-2;
-    % Temperature derivative of the salinity constants?
-    dasaldt=sal.*(4*temp.^3.*A5+3*temp.^2.*A6+2*temp.*A7+A8);
+    A5=-2.0122e-10*P-3.21e-8;
+    A6=7.988e-12*P.^2+1.0507e-8.*P+2.006e-6;
+    A7=-3.389e-13.*P.^3-1.6002e-10.*P.^2-6.4885e-8*P+7.184e-15;
+    A8=6.649e-12*P.^3+9.1041e-8*P.^2-1.258e-5*P-1.262e-2;
+    % Temperature derivative in decreasing powers of T
+    dASdT=S.*(4*T.^3.*A5+3*T.^2.*A6+2*T.*A7+A8);
 
-    B=-1.922e-2-4.42e-5*temp+(7.3637e-5+1.7945e-7*temp).*pres;
-    dbsalds=3/2*sqrt(abs(sal)).*B;
-    % Temperature derivative of the salinity constants?
-    dbsaldt=sal.*sqrt(abs(sal)).*(-4.42e-5+1.7945e-7*pres);
+    B=-1.922e-2-4.42e-5*T+(7.3637e-5+1.7945e-7*T).*P;
+    dBSdS=3/2*sqrt(abs(S)).*B;
+    % Temperature derivative in decreasing powers of P
+    dBSdT=S.*sqrt(abs(S)).*(-4.42e-5+1.7945e-7*P);
 
-    D=1.727e-3-7.9836e-6*pres;
-    ddsalds=2*sal.*D;
-    ddsaldt=0;
+    D=1.727e-3-7.9836e-6*P;
+    dDSdS=2*S.*D;
+    dDSdT=0;
 
     % Temperature and salinity derivatives of the sound speed
-    dspeeddt=sqrt(dasaldt.^2+dbsaldt.^2+dcdt.^2+ddsaldt.^2);
-    dspeedds=sqrt(dasalds.^2+dbsalds.^2+dcds.^2+ddsalds.^2);
+    dcdT=sqrt(dASdT.^2+dBSdT.^2+dCdT.^2+dDSdT.^2);
+    dcdS=sqrt(dASdS.^2+dBSdT.^2+dCdS.^2+dDSdS.^2);
 
-    % SOUND SPEED STANDARD DEVIATION
-    c=sqrt((tempSD.*dspeeddt).^2+(salSD.*dspeedds).^2);
+    % SOUND C STANDARD DEVIATION
+    c=sqrt((TSD.*dcdt).^2+(SSD.*dcds).^2);
+ case 4
+    % Del Grosso
+    % Convert input in decibar = 1e4 Pa to kg/cm^2 of Pressure for this calculation, generic
+    P=P*1.01972/10;
+    % Get the powers of the expansion
+    abc=homopoly;
+    TSP=repmat([T S P],size(abc,1),1);
+    TSPabc=prod(TSP.^abc,2);
+
+    % From Del Grosso (1974), eqs 1-6
+    % The coefficient matrix in columns of T, S, and P
+    c_abc=[ 1402.392 +0.501109398873e+1 +0.132952290781e+1 +0.156059257041e+0 ...
+	   -0.550946843172e-1 -0.127562783426e-1 +0.128955756844e-3 ...
+	   +0.635191613389e-2 0 +0.244998688441e-4 +0.221535969240e-3 ...
+	   +0.968403156410e-4 0 0 0 -0.340597039004e-3 0 -0.159349479045e-5 ...
+	   0 -0.883392332513e-8 0 0 0 -0.438031096213e-6 0 +0.485639620015e-5 ...
+	   0 +0.265484716608e-7 0 -0.161674495909e-8 +0.522116437235e-9 0];
+    % Here are those powers for your reference
+    % 0 1 0 0 2 1 0 1 0 0 3 2 1 0 2 1 0 1 0 0 3 2 1 3 2 1 0 2 1 0 1 0
+    % 0 0 1 0 0 1 2 0 1 0 0 1 2 3 0 1 2 0 1 0 1 2 3 0 1 2 3 0 1 2 0 1
+    % 0 0 0 1 0 0 0 1 1 2 0 0 0 0 1 1 1 2 2 3 0 0 0 1 1 1 1 2 2 2 3 3
+    
+    % SOUND SPEED
+    c=c_abc*TSPabc;
+ case 5
+  % Calculates the Del Grosso standard deviation via the delta method
+  tsp=repmat([T S P],size(abc,1),1);
+  % Take the power derivatives in the variables
+  dcdT=abc(:,1).^2.*([T S P].*[abc(:,1)-1 abs(:,2)   abs(:,3)]  ).^2*DT;
+  dcdS=abc(:,2).^2.*([T S P].*[abc(:,1)   abs(:,2)-1 abs(:,3)]  ).^2*DT;
+  dcdP=abc(:,3).^2.*([T S P].*[abc(:,1)   abs(:,2)   abs(:,3)-1]).^2*DT;
+
+  % SOUND SPEED VARIANCE
+  c=c_abc.^2*[dcdT dcdS dcdP];
 end
