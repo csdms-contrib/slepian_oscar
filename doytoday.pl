e877d06c1ab73065f5d40d9703c8129035191d15
Frederik J. Simons <fjsimons@gmail.com>
2023-09-20 11:06:31 -0400

    A PERL function to return today's day of year

---
+++ doytoday.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+use saclib;
+
+print "Today's day of year is: ",&dayofyearLocal(time),"\n";
--- juliantoday.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-
-use saclib;
-
-print "Today's Julian date is: ",&julianDateLocal(time),"\n";
